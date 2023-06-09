f12d92b7dfb024ae5ef10598fc0f20381cf488fb
eleazar.nascimento-at-183685660734 <eleazar.nascimento@2share.app>
2023-06-08 22:19:47 -0300

    feature: * create notification cancel use case. * configure jest paths. * add canceldAt column.

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -3,63 +3,72 @@ import { Content } from "./content";
 import { randomUUID } from "crypto";
 
 export interface NotificationProps {
-	recipientId: string;
-	content: Content;
-	category: string;
-	readAt?: Date | null;
-	createdAt: Date;
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  cancelAt?: Date | null;
+  createdAt: Date;
 }
 
 export class Notification {
-	private _id: string;
-	private props: NotificationProps;
+  private _id: string;
+  private props: NotificationProps;
 
-	constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-		this._id = randomUUID();
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
 
-		this.props = {
-			...props,
-			createdAt: props.createdAt ?? new Date(),
-		};
-	}
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
 
-	public get id() {
-		return this._id;
-	}
+  public get id() {
+    return this._id;
+  }
 
-	public set content(content: Content) {
-		this.props.content = content;
-	}
+  public set content(content: Content) {
+    this.props.content = content;
+  }
 
-	public get content(): Content {
-		return this.props.content;
-	}
+  public get content(): Content {
+    return this.props.content;
+  }
 
-	public set recipientId(recipientId: string) {
-		this.props.recipientId = recipientId;
-	}
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
 
-	public get recipientId(): string {
-		return this.props.recipientId;
-	}
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
 
-	public set category(category: string) {
-		this.props.category = category;
-	}
+  public set category(category: string) {
+    this.props.category = category;
+  }
 
-	public get category(): string {
-		return this.props.category;
-	}
+  public get category(): string {
+    return this.props.category;
+  }
 
-	public set readAt(readAt: Date | null | undefined) {
-		this.props.readAt = readAt;
-	}
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
 
-	public get readAt(): Date | null | undefined {
-		return this.props.readAt;
-	}
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
 
-	public get createdAt(): Date {
-		return this.props.createdAt;
-	}
+  public cancel() {
+    this.props.cancelAt = new Date();
+  }
+
+  public get cancelAt(): Date | null | undefined {
+    return this.props.cancelAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
 }
--- src/infra/database/prisma/prisma-notifications-repository.ts
+++ src/infra/database/prisma/prisma-notifications-repository.ts
@@ -7,14 +7,22 @@ import { PrismaNotificationMapper } from "./mappers/prisma-notification-mapper";
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
 
-    constructor(private prismaService: PrismaService) { }
+  constructor(private prismaService: PrismaService) { }
 
-    async create(notification: Notification): Promise<void> {
-        const raw = PrismaNotificationMapper.toPrisma(notification);
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error("Method not implemented.");
+  }
 
-        await this.prismaService.notification.create({
-            data: raw,
-        })
-    }
+  async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prismaService.notification.create({
+      data: raw,
+    })
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
 
 }
