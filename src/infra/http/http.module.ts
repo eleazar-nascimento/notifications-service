b786f9f9b3b1161e8e84a251f91e4e8eec4cc152
eleazar.nascimento-at-183685660734 <eleazar.nascimento@2share.app>
2023-06-25 01:37:42 -0300

    feature: implements routes http and db connection.

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,6 +2,11 @@ import { Module } from "@nestjs/common";
 import { NotificationsController } from "./controllers/notifications.controller";
 import { SendNotification } from "src/application/use-cases/send-notification";
 import { DatabaseModule } from "../database/database.module";
+import { CancelNotification } from "@application/use-cases/cancel-notification";
+import { CountRecipientNotifications } from "@application/use-cases/count-recipient-notifications";
+import { GetRecipientNotifications } from "@application/use-cases/get-recipient-notifications";
+import { ReadNotification } from "@application/use-cases/read-notification";
+import { UnreadNotification } from "@application/use-cases/unread-notification";
 
 
 @Module({
@@ -10,7 +15,12 @@ import { DatabaseModule } from "../database/database.module";
     NotificationsController
   ],
   providers: [
-    SendNotification
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
   ]
 })
 
